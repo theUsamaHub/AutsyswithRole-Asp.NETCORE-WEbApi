0e6fcb2a67a5611df90f5641490988833edf364c
usama34102 <u641332@gmail.com>
2026-03-16 12:07:08 +0500

    created a Refreshtoken in models and added in the db context

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -12,5 +12,7 @@ namespace Authsyswithrole.Data
         public DbSet<User> Users { get; set; }
 
         public DbSet<Role> Roles { get; set; }
+
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
     }
 }
---
+++ Models/RefreshToken.cs
@@ -0,0 +1,17 @@
+﻿namespace Authsyswithrole.Models
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+
+        public string Token { get; set; }
+
+        public DateTime Expires { get; set; }
+
+        public bool IsRevoked { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
