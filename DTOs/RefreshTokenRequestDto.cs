fb2bd25d7eb5a33e5ca93f50361e52b38d5be79f
usama34102 <u641332@gmail.com>
2026-03-16 12:28:19 +0500

    added RefreshTokenRequestDto.cs in DTOs folder

---
+++ DTOs/RefreshTokenRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Authsyswithrole.DTOs
+{
+    public class RefreshTokenRequestDto
+    {
+        [Required]
+        public string RefreshToken { get; set; }
+    }
+}
