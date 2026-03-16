7c0ff4a76c904040c764bfd9001cfe00b75abe2b
usama34102 <u641332@gmail.com>
2026-03-16 12:14:39 +0500

    added refresh token field in authresponseDto.cs

--- DTOs/AuthResponseDto.cs
+++ DTOs/AuthResponseDto.cs
@@ -4,6 +4,8 @@
     {
         public string Token { get; set; }
 
+        public string RefreshToken { get; set; }
+
         public int ExpiresIn { get; set; }
 
         public UserInfoDto User { get; set; }
